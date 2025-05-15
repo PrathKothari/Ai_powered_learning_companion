c251a342a98ca4822a983bb2fff0be74243ddda1
SOUMYA0722 <138036615+SOUMYA0722@users.noreply.github.com>
2025-05-15 22:18:09 +0530

    Create SectionHeading.jsx

---
+++ src/components/SectionHeading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SectionHeading = ({ children }) => (
+  <h2 className="text-xl font-bold text-gray-800 border-b border-indigo-300 pb-1 mb-4">
+    {children}
+  </h2>
+);
+
+export default SectionHeading;
